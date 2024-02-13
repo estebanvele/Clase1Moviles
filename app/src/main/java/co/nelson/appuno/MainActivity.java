53785ef1bbc7d83e9140a8565a28bae7d3959412
estebanvele <estebanvelez204@gmail.com>
2024-02-13 09:40:40 -0500

    CLASE3

--- app/src/main/java/co/nelson/appuno/MainActivity.java
+++ app/src/main/java/co/nelson/appuno/MainActivity.java
@@ -3,6 +3,7 @@ package co.nelson.appuno;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
    TextView nombreUsuario;
    Button btn1,btn2;
+
+   String Tag = "Prueba";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +24,10 @@ public class MainActivity extends AppCompatActivity {
         btn1 = findViewById(R.id.btn1);
         btn2 = findViewById(R.id.btn2);
 
-
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nombreUsuario.setText("Presioné Boton Uno");
+                nombreUsuario.setText("Presioné Boton UNO");
 
             }
         });
@@ -39,6 +41,63 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(Tag, "ESTOY EN ONSTART");
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(Tag, "ESTOY EN onResume");
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("BIENVENIDO");
+
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nombreUsuario.setText("HOLLLAA");
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onResume();
+        Log.i(Tag, "ESTOY EN onPause");
+
+    }
+
+
+
+    @Override
+    protected void onStop(){
+        super.onPause();
+        Log.i(Tag, "ESTOY EN onStop");
+
+
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(Tag, "ESTOY EN onDestroy");
+
+
+    }
+
 
     public double calcularArea(int l1,int l2){
         return l1*l2;
